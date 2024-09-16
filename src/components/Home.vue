975883976caf18f2aa9f39c091180562310203b7
divakar-srinivasan <sdivakar2005@gmail.com>
2024-09-16 12:15:50 +0530

    success

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>hellooooooo</h1>
+        <home2 />
+    </div>
+</template>
+
+<script>
+
+export default {
+    name : 'Home-components',
+    
+}
+</script>
+
+<style>
+
+</style>
